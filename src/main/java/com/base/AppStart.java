4e492ac5b1d5a6ebf005d1d40ada499ad8f9df55
tianzy <252435861@qq.com>
2017-07-05 11:58:35 +0800

    更新pom更改本地部署war位置  修改启动类 增加继承SpringBootServletInitializer 防止部署后启动找不到初始化类

--- src/main/java/com/base/AppStart.java
+++ src/main/java/com/base/AppStart.java
@@ -2,14 +2,23 @@ package com.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages="com.*")
 @ServletComponentScan//初始化过滤器CustomFilter
 @EnableTransactionManagement//如果将tomcat-jdbc排除 不会开启默认的事务管理 需手动开启
-public class AppStart {
+public class AppStart extends SpringBootServletInitializer {
 	public static void main(String[] args) throws Exception {
         SpringApplication.run(AppStart.class, args);
     }
+	
+	//防止war包部署后启动tomcat找不到初始化类
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		builder.sources(this.getClass());  
+		return super.configure(builder);
+	}
 }
