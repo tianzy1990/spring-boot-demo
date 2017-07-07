98188e3907306e3ac82c747ebc643241843fb12d
tianzy <252435861@qq.com>
2017-07-07 16:20:56 +0800

    更新注释

--- src/main/java/com/base/datasource/DataSourceAspect.java
+++ src/main/java/com/base/datasource/DataSourceAspect.java
@@ -14,15 +14,24 @@ import org.springframework.stereotype.Component;
 public class DataSourceAspect {
 	
     /**
-     * 使用空方法定义切点表达式
+     * 使用空方法定义切点表达式 针对注解  基于注解的 切入点
      */
 	@Pointcut("@annotation(com.base.datasource.tx)")
     public void tx() {
     }
 
+	/**
+	 * 设置数据源名称
+	 * @Title: setDataSourceKey
+	 * @Description: 设置数据源名称
+	 * @author tianzy
+	 * @date 2017年7月7日下午3:48:43
+	 *
+	 * @param point
+	 */
     @Before("tx()")
     public void setDataSourceKey(JoinPoint point){
-    	String args1 = point.getArgs()[0].toString();
-		DataSourceContextHolder.setDataSourceType(args1);  
+    	String args1 = point.getArgs()[0].toString();// 规定带有tx注解的方法 第一个参数是用来指定数据源名称的字段
+		DataSourceContextHolder.setDataSourceType(args1);  //设置数据源
     }
 }
--- src/main/java/com/base/datasource/DynamicDataSource.java
+++ src/main/java/com/base/datasource/DynamicDataSource.java
@@ -2,6 +2,14 @@ package com.base.datasource;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
+/**
+ * 
+ * @Title: DynamicDataSource.java
+ * @Package com.base.datasource
+ * @Description: TODO
+ * @author tianzy
+ * @date 2017年7月7日下午3:50:53
+ */
 public class DynamicDataSource extends AbstractRoutingDataSource {
 	@Override
 	protected Object determineCurrentLookupKey() {
