4163ba650d335ba90acbafcf18af1f8e5db2623b
tianzy <252435861@qq.com>
2017-09-07 16:54:36 +0800

    增加jsoup.jar 生成汉子方法

---
+++ src/main/java/com/tool/CreatHanZi.java
@@ -0,0 +1,31 @@
+package com.tool;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+public class CreatHanZi {
+	public static String getRandomJianHan(int len) {
+		String ret = "";
+		for (int i = 0; i < len; i++) {
+			String str = null;
+			int hightPos, lowPos; // 定义高低位
+			Random random = new Random();
+			hightPos = (176 + Math.abs(random.nextInt(39))); // 获取高位值
+			lowPos = (161 + Math.abs(random.nextInt(93))); // 获取低位值
+			byte[] b = new byte[2];
+			b[0] = (new Integer(hightPos).byteValue());
+			b[1] = (new Integer(lowPos).byteValue());
+			try {
+				str = new String(b, "GBk"); // 转成中文
+			} catch (UnsupportedEncodingException ex) {
+				ex.printStackTrace();
+			}
+			ret += str;
+		}
+		return ret;
+	}
+	public static void main(String[] args) {
+		String randomJianHan = getRandomJianHan(1);
+		System.out.println(randomJianHan);
+	}
+}
